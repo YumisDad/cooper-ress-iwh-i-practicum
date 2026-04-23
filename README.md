9237132ca541c4c2081bf854cb096b758a349729
Cooper Ress <cooperress@gmail.com>
2026-04-22 22:43:37 -0400

    Complete HubSpot practicum app: routes, views, and config

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
 # Welcome to the Integrating With HubSpot I: Foundations Practicum
 
-This repository is for the Integrating With HubSpot I: Foundations course. This practicum is one of two requirements for receiving your Integrating With HubSpot I: Foundations certification. You must also take the exam and receive a passing grade (at least 75%).
+This repository is for the Integrating With HubSpot I: Foundations course. This practicum is one of two requirements for receiving your Integrating With HubSpot I: Foundations certification. You must also take the exam and receive a passing grade of at least 75%.
 
-To read the full directions, please go to the [practicum instructions](https://app.hubspot.com/academy/l/tracks/1092124/1093824/5493?language=en).
+To read the full directions, please go to the practicum instructions in HubSpot Academy.
 
 Initial setup for Integrating With HubSpot I: Foundations practicum.
 
-**Put your HubSpot developer test account custom objects URL link here: https://app-na2.hubspot.com/contacts/245985237/objects/2-228619004/views/all/list** 
+## HubSpot Custom Object List View
+
+https://app-na2.hubspot.com/contacts/245985237/objects/2-228619004/views/all/list
 
 ___
-## Tips:
-- Commit to your repository often. Even if you make small tweaks to your code, it’s best to be committing to your repository frequently.
-- The subject of the custom object is up to you. Feel free to get creative!
-- Please create a test account and include your private app access token in your repo.
-- Ensure you re-merge any working branches into the main branch.
-- DO NOT ADD YOUR PRIVATE APP TOKEN TO YOUR REPOSITORY. 
 
-## Pre-requisites:
-- Using [Node](https://nodejs.org/en/download) and node packages
-- Using [Express](https://expressjs.com/en/starter/installing.html)
-- Using [Axios](https://axios-http.com/docs/intro)
-- Using [Pug templating system](https://pugjs.org/api/getting-started.html)
+## Tips
+- Commit to your repository often. Even if you make small tweaks to your code, it’s best to be committing to your repository frequently.
+- The subject of the custom object is up to you. Feel free to get creative.
+- Please create a test account, but do not include your private app access token in your repo.
+- Ensure you re-merge any working branches into the main branch.
+
+## Pre-requisites
+- Using Node and node packages
+- Using Express
+- Using Axios
+- Using the Pug templating system
 - Using the command line
-- Using [Git and GitHub](https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners)
+- Using Git and GitHub
 
 ## Requirements
-- All work must be your own. During the grading process we will check the revision history. Submissions that do not meet this requirement will not be considered.
-- You must have at least two new routes in your index.js file and one new pug template for the homepage.
-- You must create a developer test account and link to it in your README.md file. Submissions that do not meet this requirement will not be considered.
+- All work must be your own. During the grading process, revision history may be reviewed.
+- You must have at least three new routes in your `index.js` file.
+- You must have two new Pug templates: one for the homepage and one for the form.
+- You must create a developer test account and link to the custom object list page in your `README.md`.
--- index.js
+++ index.js
@@ -1,71 +1,106 @@
+// Load environment variables from .env
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
 
+// Load env vars
+const PORT = process.env.PORT || 3000;
+const HUBSPOT_PRIVATE_APP_TOKEN = process.env.HUBSPOT_PRIVATE_APP_TOKEN;
+const HUBSPOT_ACCOUNT_ID = process.env.HUBSPOT_ACCOUNT_ID;
+const HUBSPOT_CUSTOM_OBJECT_TYPE = process.env.HUBSPOT_CUSTOM_OBJECT_TYPE;
+
+// Basic safety check for required env vars
+if (!HUBSPOT_PRIVATE_APP_TOKEN || !HUBSPOT_CUSTOM_OBJECT_TYPE) {
+  console.warn('Missing HubSpot env vars. Check your .env file.');
+}
+
+// Express configuration
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+// Axios instance for HubSpot API
+const hubspotApi = axios.create({
+  baseURL: 'https://api.hubapi.com',
+  headers: {
+    Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_TOKEN}`,
+    'Content-Type': 'application/json'
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
+// 1) Homepage route ("/") – list custom object records
+app.get('/', async (req, res) => {
+  try {
+    const limit = 50;
+    const properties = ['name', 'species', 'bio']; // update if your property internal names differ
+
+    const response = await hubspotApi.get(
+      `/crm/v3/objects/${HUBSPOT_CUSTOM_OBJECT_TYPE}`,
+      {
+        params: {
+          limit,
+          properties: properties.join(',')
         }
-    }
+      }
+    );
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
+    const records = response.data.results || [];
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+    res.render('homepage', {
+      title: 'Custom Object Records | Integrating With HubSpot I Practicum',
+      records,
+      properties
+    });
+  } catch (error) {
+    console.error(
+      'Error fetching custom object records from HubSpot:',
+      error.response?.data || error.message
+    );
+    res.status(500).send('Error fetching records from HubSpot');
+  }
 });
-*/
 
+// 2) GET "/update-cobj" – render the form
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+  });
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+// 3) POST "/update-cobj" – create new record and redirect home
+app.post('/update-cobj', async (req, res) => {
+  const { name, species, bio } = req.body; // field names must match your form inputs
+
+  const data = {
+    properties: {
+      name,
+      species,
+      bio
+    }
+  };
+
+  try {
+    await hubspotApi.post(
+      `/crm/v3/objects/${HUBSPOT_CUSTOM_OBJECT_TYPE}`,
+      data
+    );
+    res.redirect('/');
+  } catch (error) {
+    console.error(
+      'Error creating custom object record in HubSpot:',
+      error.response?.data || error.message
+    );
+    res.status(500).send('Error creating record in HubSpot');
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
